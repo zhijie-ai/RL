a0d654355c9b231343e43747021380b89ba774bd
zushoujie <zzj_ai@163.com>
2020-10-20 15:26:12 +0800

    top k 论文实现 增加使用movie-length数据集代码

--- rl-rec-practice/topk/code/TopKReinforce.py
+++ rl-rec-practice/topk/code/TopKReinforce.py
@@ -11,6 +11,7 @@
 #----------------------------------------------
 import tensorflow as tf
 import numpy as np
+import pandas as pd
 import time
 import math
 import pickle
@@ -51,6 +52,46 @@ def load_data(path='../data/session.pickle',time_step=7,gamma=0.95):
                 historys.append(list(t[i:i+time_step]))
                 actions.append(t[i+time_step])
                 rewards.append(r[i+time_step])
+
+
+    return np.array(historys),np.array(actions),np.array(rewards)
+
+def load_data_movie_length(path='../data/ratings.dat',time_step=7,gamma=.9):
+    historys=[]
+    actions=[]
+    rewards=[]
+
+    def _discount_and_norm_rewards(rewards):
+        discounted_episode_rewards = np.zeros_like(rewards,dtype='float64')
+        cumulative = 0
+        for t in reversed(range(len(rewards))):
+            cumulative = cumulative * gamma + rewards[t]
+            discounted_episode_rewards[t] = cumulative
+        # Normalize the rewards
+        #discounted_episode_rewards -= np.mean(discounted_episode_rewards)
+        #discounted_episode_rewards /= np.std(discounted_episode_rewards)
+        return discounted_episode_rewards
+
+    ratings = pd.read_csv(path,delimiter='::',index_col=None,header=None,names=['userid','itemid','rating','timestamp'])
+    print(ratings.head())
+
+    items = list(sorted(ratings.itemid.unique()))
+    key_to_id_item = dict(zip(items,range(len(items))))
+    id_to_key_item = dict(zip(range(len(items)),items))
+    users = list(set(sorted(ratings.userid.unique())))
+    key_to_id_user = dict(zip(users,range(len(users))))
+    id_to_key_user = dict(zip(range(len(users)),users))
+
+    ratings.userid = ratings.userid.map(key_to_id_user)
+    ratings.itemid = ratings.itemid.map(key_to_id_item)
+    ratings = ratings.sort_values(by=['timestamp']).drop('timestamp',axis=1).groupby('userid')
+    for _,df in ratings:
+        r = _discount_and_norm_rewards(df.rating.values)
+        items = df.itemid.values
+        for i in range(len(items)-time_step):
+            historys.append(list(items[i:i+time_step]))
+            actions.append(items[i+time_step])
+            rewards.append(r[i+time_step])
 
 
     return np.array(historys),np.array(actions),np.array(rewards)
