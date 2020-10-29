56dd10ef45a3d69b70df6c981005cc21a490e5a4
zushoujie <zzj_ai@163.com>
2020-10-29 12:27:28 +0800

    reward 回归模型

--- rl-rec-practice/topk/code/Reward_keras.py
+++ rl-rec-practice/topk/code/Reward_keras.py
@@ -2,14 +2,13 @@
 # -*- encoding=utf-8 -*-                       #
 # __author__:'焉知飞鱼'                         #
 # CreateTime:                                  #
-#       2020/10/22 15:56                         #
+#       2020/10/29 9:55                         #
 #                                              #
 #               天下风云出我辈，                 #
 #               一入江湖岁月催。                 #
 #               皇图霸业谈笑中，                 #
 #               不胜人生一场醉。                 #
 # -----------------------------------------------
-# reward的预测
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
@@ -18,6 +17,10 @@ from tensorflow.keras import backend as K
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import Input, Dense, LSTM, Embedding, concatenate
+from tensorflow.keras.models import Model
+from tensorflow.keras.utils import plot_model
 
 
 def load_data_movie_length(path='../data/ratings.dat', time_step=15, gamma=.9):
@@ -61,59 +64,69 @@ def load_data_movie_length(path='../data/ratings.dat', time_step=15, gamma=.9):
     return np.array(historys), np.array(actions), np.array(rewards)
 
 
-class Reward(keras.Model):
-    def __init__(self, embedding_size=64, epochs=1000, item_count=6040, unit=128, time_step=15):
+class Reward():
+    def __init__(self, embedding_size=64, epochs=1000, item_count=6040, unit=128, time_step=15, gamma=0.9,
+                 batch_size=512):
         self.embedding_size = embedding_size
-        self.epochs = epochs
         self.item_count = item_count
         self.time_step = time_step
         self.units = unit
+        self.gamma = gamma
+        self.epochs = epochs
+        self.batch_size = batch_size
 
-        ipt1 = keras.Input(shape=(self.time_step,), name="history")
-        ipt2 = keras.Input(shape=(1,), name='actions')
+        self.historys, self.actions, self.rewards = load_data_movie_length(time_step=self.time_step, gamma=self.gamma)
 
-        # action
-        action = keras.layers.Embedding(self.item_count, self.embedding_size, input_length=self.time_step)(ipt2)
+        optimizer = Adam(0.0002, 0.5)
+
+        # Build and compile the model
+        self.model = self.build_model()
+
+        self.model.compile(optimizer=optimizer,
+                           loss=keras.losses.MeanSquaredError(),
+                           metrics=['mae'])
+
+    def build_model(self):
+        inp1 = Input(shape=(self.time_step,), name='history')
+        inp2 = Input(shape=(1,), name='action')
+
+        action = Embedding(self.item_count, self.embedding_size, input_length=self.time_step)(inp2)
         print('action------------', action.shape)
         action = K.squeeze(action, axis=1)
         # action = K.reshape(action,(-1,self.embedding_size))
         print('action', action.shape)
-        x = keras.layers.Embedding(self.item_count, self.embedding_size, input_length=self.time_step)(ipt1)
-        x = keras.layers.LSTM(units=self.units)(x)
+        x = Embedding(self.item_count, self.embedding_size, input_length=self.time_step)(inp1)
+        x = LSTM(units=self.units)(x)
         print('x.shape', x.shape)
-        x = keras.layers.concatenate([action, x])  # https://zhuanlan.zhihu.com/p/81721574
+        x = concatenate([action, x])
         print('AAAA', x.shape)
-        x = keras.layers.Dense(64)(x)
-        out = keras.layers.Dense(1)(x)
-        super(Reward, self).__init__(inputs=[ipt1, ipt2], outputs=out)
+        x = Dense(64)(x)
+        out = Dense(1)(x)
 
+        model = Model([inp1, inp2], out, name='reward_model')
 
-def main():
-    historys, actions, rewards = load_data_movie_length()
-    historys_train, historys_val, action_train, action_val, rewards_train, rewards_val = train_test_split(historys,
-                                                                                                          actions,
-                                                                                                          rewards,
-                                                                                                          test_size=0.2)
+        plot_model(model, to_file='./png/model.png', show_shapes=True)
+        return model
 
-    model = Reward()
-    # model.summary()
+    def train(self):
+        historys_train, historys_val, action_train, action_val, rewards_train, rewards_val = train_test_split(
+            self.historys, self.actions,
+            self.rewards, test_size=0.2)
+        print('GGGGGGGG', historys_train.shape, historys_val.shape, action_train.shape, action_val.shape,
+              rewards_train.shape, rewards_val.shape)
+        filepath = "weights.best.hdf5"
 
-    model.compile(optimizer=keras.optimizers.Adam(0.001),
-                  loss=keras.losses.MeanSquaredError(),
-                  metrics=['mae'])
-    print('EEEEE',model.metrics_names)
-    filepath = "weights.best.hdf5"
+        ckp = ModelCheckpoint(filepath, save_best_only=True, verbose=1,monitor='val_mae')
+        stop = EarlyStopping(patience=10, verbose=1)
 
-    ckp = ModelCheckpoint(filepath, save_best_only=True, verbose=1,monitor='val_mae')
-    stop = EarlyStopping(patience=10, verbose=1)
-
-    # train
-    model.fit([historys_train, action_train], rewards_train,
-              epochs=1000, batch_size=512, verbose=1,
-              validation_data=([historys_val, action_val], rewards_val),
-              shuffle=True,
-              callbacks=[stop,ckp])
+        # train
+        self.model.fit([historys_train, action_train], rewards_train,
+                       epochs=self.epochs,
+                       batch_size=self.batch_size, verbose=1, shuffle=True,
+                       validation_data=([historys_val,action_val],rewards_val),
+                       callbacks=[ckp,stop])
 
 
 if __name__ == '__main__':
-    main()
+    reward = Reward(epochs=1)
+    reward.train()
