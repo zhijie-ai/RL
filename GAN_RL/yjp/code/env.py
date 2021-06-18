674412ad324f45da3b1827b0f9c26b31e1c2a080
zushoujie <zushoujie@yijiupi.com>
2021-06-18 18:22:58 +0800

    解决np.round无法四舍五入之类的问题

--- GAN_RL/yjp/code/env.py
+++ GAN_RL/yjp/code/env.py
@@ -291,8 +291,8 @@ class Enviroment():
             prob = prob / float(prob.sum())
             rand_choice = np.random.choice(disp_item + [-100], 1, p=prob)
             if j<=100:
-                print('AAAA:{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}'.format(np.round(np.max(reward_u[j])),np.round(np.max(np.exp(reward_u[j]))),
-                                                                       np.round(np.sum(transition_p[j,:]),3),np.round(no_click,3),
+                print('AAAA:{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}'.format(np.round(np.sort(reward_u[j][-1:]),3),np.round(np.sort(np.exp(reward_u[j])[-1:]),3),
+                                                                       np.round([np.sum(transition_p[j,:])],3),np.round(no_click,3),
                                                                        np.round(np.sort(prob[:-1])[-4:],3),np.round(np.sort(np.exp(reward_u[j]))[-4:],3),
                                                                        np.round(np.sort(reward_u[j])[-4:],3),np.round(prob[-1],3),rand_choice))
 
