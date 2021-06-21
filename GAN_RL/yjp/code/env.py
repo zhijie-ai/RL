8e3e83adf3e71227f23ed28492bee9bd1c911885
zushoujie <zushoujie@yijiupi.com>
2021-06-21 10:49:55 +0800

    保存greedy和random的data_collection数据

--- GAN_RL/yjp/code/env.py
+++ GAN_RL/yjp/code/env.py
@@ -291,7 +291,6 @@ class Enviroment():
             prob = prob / float(prob.sum())
             rand_choice = np.random.choice(disp_item + [-100], 1, p=prob)
             if j<=100:
-                print('BBB',reward_u[j])
                 print('AAAA:{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}'.format(np.max(reward_u[j]),np.round(np.sort(np.exp(reward_u[j]))[-1:],3),
                                                                        np.round([np.sum(transition_p[j,:])],3),np.round(no_click,3),
                                                                        np.round(np.sort(prob[:-1])[-4:],3),np.round(np.sort(np.exp(reward_u[j]))[-4:],3),
